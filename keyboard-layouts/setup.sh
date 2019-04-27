47b8f752d3660ed82d4f54b63c561e65edcbe298
Kaspar Manz <kaspar@xeophin.net>
2019-04-27 09:04:32 +0200

    fix: follow convention of other setup files

--- keyboard-layouts/setup.sh
+++ keyboard-layouts/setup.sh
@@ -10,7 +10,7 @@ DESTINATION="$(realpath /Library/Keyboard\ Layouts/)"
 
 info "Configuring keyboard layouts..."
 mkdir -p "${DESTINATION}"
-find * -maxdepth 0 -not -name "setup.sh" | while read fn; do
+find * -maxdepth 0 -not -name "$(basename ${0})" | while read fn; do
     fn=$(basename "${fn}")
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
