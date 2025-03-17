6647db5f483dc0decfc1cce37dbb2f7f70e9f34a
Kaspar Manz <code@xeophin.net>
2025-03-17 11:30:29 +0100

    feat: add nushell config
    Closes: XEO-478

---
+++ nushell/setup.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR" || exit
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath .)"
+DESTINATION="$(realpath '~/Library/Application Support/nushell')"
+
+info "Configuring services..."
+mkdir -p "${DESTINATION}"
+find * -maxdepth 0 -not -name "$(basename ${0})" | while read fn; do
+    fn=$(basename "${fn}")
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+success "Finished configuring services."
